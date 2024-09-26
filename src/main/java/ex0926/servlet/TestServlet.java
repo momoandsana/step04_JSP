67c245c25d0ee40f801e63eb3c3d31ecd96c257c
hoseok <swiftie52@naver.com>
2024-09-26 14:03:46 +0900

    filter 관련 코드 추가

---
+++ src/main/java/ex0926/servlet/TestServlet.java
@@ -0,0 +1,20 @@
+package ex0926.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(loadOnStartup = 1,urlPatterns = "/test")
+public class TestServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("여기는 TestServlet 입니다");
+    }
+}
---
+++ src/main/webapp/test02.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: swift
+  Date: 2024-09-26
+  Time: 오후 12:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>test02.jsp 요청되었습니다</h1>
+<%
+    //콘솔 출력
+    System.out.println("test02.jsp 입니다...");
+
+%>
+</body>
+</html>
